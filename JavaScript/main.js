ead7a337af00d217e3727d98bfad182f2e130157
Nicholas Glover <jacksaintjack@gmail.com>
2015-10-26 15:21:04 -0500

    Finished for the most part, but would like to change how I do the color change on the buttons

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,146 +1,56 @@
-///Clear Button
-clearSelect.addEventListener('click', function(){
-  displayArea.innerText = 0;
-  return inputValues = [];
-});
+var inputValues = [];
+var answer;
+var displayArea = document.getElementById('displayArea');
+var inputDisplay;
+var displayReturn = function(){
+  inputDisplay = inputValues.join("");
+  return inputDisplay;
+};
+var handlerSelect;
+var backgroundHandler = function(){
+  handlerSelect.style.background = 'rgba(104, 178, 91, 0.7)';
+  setTimeout(function(){
+  handlerSelect.style.background = '#68B25B';
+},100);
+};
 
-///Row Two Buttons Functions
-sevenSelect.addEventListener('click', function(){
-  displayReturn(inputValues.push(7));
+var colorHandler = function(){
+  handlerSelect.style.color = 'white';
+  setTimeout(function(){
+  handlerSelect.style.color = 'black';
+},100);
+};
+
+var clickNumber = function(){
+  inputValues.push($(this).html());
+  displayReturn();
   displayArea.innerText = inputDisplay;
-  handlerSelect = sevenSelect;
+  handlerSelect = this;
   backgroundHandler();
-});
+}
 
-eightSelect.addEventListener('click', function(){
-  displayReturn(inputValues.push(8));
+var clickOperator = function(){
+  inputValues.push($(this).html());
+  displayReturn()
   displayArea.innerText = inputDisplay;
-  handlerSelect = eightSelect;
-  backgroundHandler();
-});
-
-nineSelect.addEventListener('click', function(){
-  displayReturn(inputValues.push(9));
-  displayArea.innerText = inputDisplay;
-  handlerSelect = nineSelect;
-  backgroundHandler();
-});
-
-divideSelect.addEventListener('click', function(){
-  displayReturn(inputValues.push("/"));
-  displayArea.innerText = inputDisplay;
-  handlerSelect = divideSelect;
+  handlerSelect = this;
   colorHandler();
+}
+
+
+$('.number').click(clickNumber);
+$('.operator').click(clickOperator);
+
+$("#equal").click(function(){
+  answer = eval(inputDisplay)
+  function emptyArray(){
+    inputValues = [];
+  }
+  function lastAnswer(){
+    inputValues.push(answer);
+  }
+  emptyArray();
+  lastAnswer();
+  displayArea.innerText = answer;
+  return answer;
 });
-
-///Row Three Buttons Functions
-fourSelect.addEventListener('click', function(){
-  displayReturn(inputValues.push(4));
-  displayArea.innerText = inputDisplay;
-  handlerSelect = fourSelect;
-  backgroundHandler();
-});
-
-fiveSelect.addEventListener('click', function(){
-  displayReturn(inputValues.push(5));
-  displayArea.innerText = inputDisplay;
-  handlerSelect = fiveSelect;
-  backgroundHandler();
-});
-sixSelect.addEventListener('click', function(){
-  displayReturn(inputValues.push(6));
-  displayArea.innerText = inputDisplay;
-  handlerSelect = sixSelect;
-  backgroundHandler();
-});
-
-multiplySelect.addEventListener('click', function(){
-  displayReturn(inputValues.push("*"));
-  displayArea.innerText = inputDisplay;
-  handlerSelect = multiplySelect;
-  colorHandler();
-});
-
-///Row Four Buttons Functions
-oneSelect.addEventListener('click', function(){
-  displayReturn(inputValues.push(1));
-  displayArea.innerText = inputDisplay;
-  handlerSelect = oneSelect;
-  backgroundHandler();
-});
-
-twoSelect.addEventListener('click', function(){
-  displayReturn(inputValues.push(2));
-  displayArea.innerText = inputDisplay;
-  handlerSelect = twoSelect;
-  backgroundHandler();
-});
-
-threeSelect.addEventListener('click', function(){
-  displayReturn(inputValues.push(3));
-  displayArea.innerText = inputDisplay;
-  handlerSelect = threeSelect;
-  backgroundHandler();
-});
-
-subtractSelect.addEventListener('click', function(){
-  displayReturn(inputValues.push("-"));
-  displayArea.innerText = inputDisplay;
-  handlerSelect = subtractSelect;
-  colorHandler();
-
-});
-
-///Row Four Buttons Functions
-zeroSelect.addEventListener('click', function(){
-  displayReturn(inputValues.push(0));
-  displayArea.innerText = inputDisplay;
-  handlerSelect = zeroSelect;
-  backgroundHandler();
-});
-
-decimalSelect.addEventListener('click', function(){
-  displayReturn(inputValues.push("."));
-  displayArea.innerText = inputDisplay;
-  handlerSelect = decimalSelect;
-  backgroundHandler();
-});
-
-
-addSelect.addEventListener('click', function(){
-  displayReturn(inputValues.push("+"));
-  displayArea.innerHTML = inputDisplay;
-  handlerSelect = addSelect;
-  colorHandler();
-});
-
-//The equal function. evals the array
-
-
-equalSelect.addEventListener('click', function(){
-    handlerSelect = equalSelect;
-    backgroundHandler();
-
-    function emptyArray(){
-      inputValues = [];
-    }
-
-    function lastAnswer(){
-      inputValues.push(answer);
-    }
-    try {
-      answer = eval(inputDisplay);
-    } catch (error) {
-      if(answer === undefined){
-        answer = "error";
-        return answer;
-      }
-    }
-        emptyArray();
-        lastAnswer();
-        displayArea.innerText = answer;
-        return answer;
-});
-
-
-/// var digits = documents.getElementsByclassName('digit')
